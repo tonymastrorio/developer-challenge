b7439e15e3c95b055d2f4b649554b94369482169
Tony <github email address>
2017-07-21 13:22:16 -0400

    add form error styling

--- js/startup.js
+++ js/startup.js
@@ -6,9 +6,11 @@ $(document).ready(function() {
 		var name = $("#name").val();
 	   		if (name != "") {
 	   			$("#name-error").html("");
+	   			$(this).removeClass("error-input");
 	   			errors = false;
 	   		} else {
-	   			$("#name-error").html("Your name is required.");
+	   			$("#name-error").html("Your name is required");
+	   			$(this).addClass("error-input");
 	   			errors = true;
 	   		}
 		});
@@ -17,9 +19,11 @@ $(document).ready(function() {
 		var email = $("#email").val();
 	   		if (/(..+)@(.+)\.[a-zA-Z0-9]{2,}$/.test(email)) {
 	   			$("#email-error").html("");
+	   			$(this).removeClass("error-input");
 	   			errors = false;
 	   		} else {
 	   			$("#email-error").html("Please enter a valid email address");
+	   			$(this).addClass("error-input");
 	   			errors = true;
 	   		}
 		});
@@ -35,10 +39,12 @@ $(document).ready(function() {
 		for (var i=0; i<patterns.length; i++) {
 			if (phone.indexOf(patterns[i]) >= 0) {
 				$("#phone-error").html("Please enter a valid phone number");
+				$(this).addClass("error-input");
 				errors = true;
 				return false;
 			} else {
 				$("#phone-error").html("");
+				$(this).removeClass("error-input");
 				errors = false;
 			}
 
@@ -47,19 +53,23 @@ $(document).ready(function() {
 		for (var i=0; i<starts.length; i++) {
 			if (phone.startsWith(starts[i])) {
 				$("#phone-error").html("Please enter a valid phone number");
+				$(this).addClass("error-input");
 				errors = true;
 				return false;
 			} else {
 				$("#phone-error").html("");
+				$(this).removeClass("error-input");
 				errors = false;
 			}
 		}
 
    		if (/^\d{10,15}$/.test(phone)) {
    			$("#phone-error").html("");
+   			$(this).removeClass("error-input");
    			errors = false;
    		} else {
    			$("#phone-error").html("Please enter a valid phone number");
+   			$(this).addClass("error-input");
    			errors = true;
    		}
 	});
@@ -71,3 +81,4 @@ $(document).ready(function() {
 	});
 
 });
+
